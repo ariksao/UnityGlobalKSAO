6092e7d6158fcabed11e070aac77a3cdd7c4333c
ariksao <73885398+ariksao@users.noreply.github.com>
2020-11-25 15:44:34 +0200

    Some small actions
    Added FBX mesh with animation on the scene. New material. Public GameObject with rotation and Blender animation imported. Works so far..

--- UnityGitA/Assets/TheCode.cs
+++ UnityGitA/Assets/TheCode.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class TheCode : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject kikkare;
+    public float kikkare_speed;
     void Start()
     {
         Debug.Log("Testing Git!");
         Debug.Log("This line was added with Notepad++");
         Debug.Log("This line was added with Visual Studio 2019"); //Typo fixed
-	Debug.Log("This line was added with Microsoft Notepad from another computer");
+        Debug.Log("This line was added with Microsoft Notepad from another computer");
+        Debug.Log("Testing New Visual Studio from remote PC");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // Try to rotate gameobject by its X -axis
+        kikkare.transform.Rotate(0, kikkare_speed * Time.deltaTime, 0);
     }
 }
