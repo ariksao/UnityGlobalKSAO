86e9c8b2871173dfaf9fca75b4ee1350a3f3deb4
ariksao <73885398+ariksao@users.noreply.github.com>
2020-11-25 14:22:18 +0200

    Reposity cloned and some small edits made to c# code
    Edited code with MS-Notepad and other PC. Unity project opened and worked well..

--- UnityGitA/Assets/TheCode.cs
+++ UnityGitA/Assets/TheCode.cs
@@ -10,6 +10,7 @@ public class TheCode : MonoBehaviour
         Debug.Log("Testing Git!");
         Debug.Log("This line was added with Notepad++");
         Debug.Log("This line was added with Visual Studio 2019"); //Typo fixed
+	Debug.Log("This line was added with Microsoft Notepad from another computer");
     }
 
     // Update is called once per frame
