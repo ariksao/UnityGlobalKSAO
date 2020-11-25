055ad6b4ac2ca5076a8d3d367a0fa29de7ad4ad6
ariksao <73885398+ariksao@users.noreply.github.com>
2020-11-25 10:14:42 +0200

    Some code editing..
    Edit with Notepad++

---
+++ UnityGitA/Assets/TheCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Testing Git!");
+		Debug.Log("This line was added with Notepad++");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
