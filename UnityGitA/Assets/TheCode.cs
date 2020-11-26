19bfdfe8476e345c4b94f6f4b138b5e2b63fa0f2
ariksao <73885398+ariksao@users.noreply.github.com>
2020-11-26 11:13:53 +0200

    Some tests from 3th workstation
    Just testing Git

--- UnityGitA/Assets/TheCode.cs
+++ UnityGitA/Assets/TheCode.cs
@@ -14,12 +14,13 @@ public class TheCode : MonoBehaviour
         Debug.Log("This line was added with Visual Studio 2019"); //Typo fixed
         Debug.Log("This line was added with Microsoft Notepad from another computer");
         Debug.Log("Testing New Visual Studio from remote PC");
+        Debug.Log("This line was added from 3th workstation with Visual Studio");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Try to rotate gameobject by its X -axis
+        // Rotates gameobject by its X -axis
         kikkare.transform.Rotate(0, kikkare_speed * Time.deltaTime, 0);
     }
 }
